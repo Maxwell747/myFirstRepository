4850b5a0fc74122771a4bd11491c8098553bb4cd
Maxwell Slingerland <55295503+Maxwell747@users.noreply.github.com>
2021-02-02 20:57:00 -0700

    Add files via upload

---
+++ mslinger_hw2 .sh
@@ -0,0 +1,28 @@
+﻿#!/bin/bash
+
+if [ $# -eq 0]; then
+    echo "usage: modify filename [filename ...]"
+    exit 0
+fi
+
+date_time = $ (date "+%A, %b %d, %Y: %H%M")
+
+for f in "$@"/*
+do
+    if [[ -d "$f" ]]; then
+        echo "modify: '$f', is a directory."
+    elif [[ -x "$f" ]]; then
+        echo "modify: '$f', cannot edit executables."
+    elif [ "$(file "$f")" = "$f: ASCII text" ]; then
+        ed <<- EOF > $f
+        Modified on: $date_time
+        Modified by: Maxwell Slingerland
+        EOF
+        ed $f
+        2m0
+        s/Gradez/Grades
+        w
+        q
+    else
+        echo "modify: '$f', filetype not supported."
+done
